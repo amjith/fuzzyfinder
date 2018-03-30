b8fed3e8453b052564f00428708a63b135d72220
Gokul <gokulps15@gmail.com>
2018-03-30 22:22:02 +0530

    added parameter to control alphanumeric sorting

--- fuzzyfinder/main.py
+++ fuzzyfinder/main.py
@@ -3,7 +3,7 @@ import re
 from . import export
 
 @export
-def fuzzyfinder(input, collection, accessor=lambda x: x):
+def fuzzyfinder(input, collection, accessor=lambda x: x, alpha_num_sort=True):
     """
     Args:
         input (str): A partial string which is typically entered by a user.
@@ -12,6 +12,11 @@ def fuzzyfinder(input, collection, accessor=lambda x: x):
         accessor (function): If the `collection` is not an iterable of strings,
                              then use the accessor to fetch the string that
                              will be used for fuzzy matching.
+        alpha_num_sort(bool): The suggestions are sorted by considering the
+                              smallest contiguous match, followed by where the
+                              match is found in the full string, followed by
+                              alphanumeric sorting. If this parameter is
+                              `False`, the alphanumeric sorting is not done.
 
     Returns:
         suggestions (generator): A generator object that produces a list of
@@ -28,4 +33,12 @@ def fuzzyfinder(input, collection, accessor=lambda x: x):
             best = min(r, key=lambda x: len(x.group(1)))   # find shortest match
             suggestions.append((len(best.group(1)), best.start(), accessor(item), item))
 
-    return (z[-1] for z in sorted(suggestions))
+    if alpha_num_sort:
+        return (z[-1] for z in sorted(suggestions))
+    else:
+        if isinstance(collection[0], str):
+            # Items of `collection` are strings; they would be used for
+            # alphanumeric sorting. Hence remove them when sorting
+            return (z[-1] for z in sorted(suggestions, key=lambda x: x[:2]))
+        else:
+            return (z[-1] for z in sorted(suggestions, key=lambda x: (x[0], x[1], x[3])))
