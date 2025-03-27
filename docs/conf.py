29adf49830062fd88dec59410bcb8af3b56b8960
Nace Zavrtanik <zavrtaniknace@gmail.com>
2025-03-27 08:21:59 +0100

    Sphinx now uses the RTD theme, falls back to default if not available

--- docs/conf.py
+++ docs/conf.py
@@ -15,6 +15,7 @@
 
 import sys
 import os
+import importlib.metadata
 
 # If extensions (or modules to document with autodoc) are in another
 # directory, add these directories to sys.path here. If the directory is
@@ -111,7 +112,12 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'default'
+html_theme = 'sphinx_rtd_theme'
+try:
+    importlib.metadata.version(html_theme)
+except importlib.metadata.PackageNotFoundError:
+    print(f"HTML theme '{html_theme}' not installed, falling back to 'default'")
+    html_theme = 'default'
 
 # Theme options are theme-specific and customize the look and feel of a
 # theme further.  For a list of options available for each theme, see the
