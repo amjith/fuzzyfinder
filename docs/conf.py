92c911a473d969bb587c853ebc09281c2ba894fe
Nace Zavrtanik <zavrtaniknace@gmail.com>
2025-03-27 17:08:09 +0100

    Proper content added to the API Reference section
    - Expanded docstring of fuzzyfinder function.
    - Updated type hints to allow for iterables of non-strings.

--- docs/conf.py
+++ docs/conf.py
@@ -41,7 +41,13 @@ import fuzzyfinder
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode', 'sphinx.ext.napoleon']
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autodoc.typehints',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.napoleon',
+]
+autodoc_typehints = 'description'
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
