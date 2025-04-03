e31f2af3b1314cde9cd6e1cd3c0dc0df3f288c2f
Nace Zavrtanik <zavrtaniknace@gmail.com>
2025-04-03 22:59:26 +0200

    Type hints added to fuzzyfinder/__init__.py

--- fuzzyfinder/__init__.py
+++ fuzzyfinder/__init__.py
@@ -5,7 +5,10 @@ __version__ = "2.2.0"
 __all__ = []
 
 
-def export(defn):
+from typing import Callable
+
+
+def export(defn: Callable) -> Callable:
     """Decorator to explicitly mark functions that are exposed in a lib."""
     globals()[defn.__name__] = defn
     __all__.append(defn.__name__)
